5c299fc9e1eaa996c52bd4ff66161db23037af6a
jn <jnoncode@gmail.com>
2024-11-16 15:11:05 +0900

    feat: project setting

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+type ConditionalClass = {
+  if: boolean;
+  classes: string;
+};
+
+type StyleConfig = {
+  [key: string]: string | ConditionalClass;
+};
+
+export function tw(config: StyleConfig): string {
+  const classes: string[] = [];
+
+  for (const key in config) {
+    const value = config[key];
+
+    if (typeof value === "string") {
+      classes.push(value);
+    } else if (typeof value === "object" && value.if) {
+      classes.push(value.classes);
+    }
+  }
+
+  return classes.join(" ");
+}
